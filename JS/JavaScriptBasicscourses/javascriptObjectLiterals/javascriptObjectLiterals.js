4edc30cdaa74fe6c53ee163f902d178a6aa695d9
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-06-08 21:04:27 +0900

    complete js repeating stuff with loops

--- JS/JavaScriptBasicscourses/javascriptObjectLiterals/javascriptObjectLiterals.js
+++ JS/JavaScriptBasicscourses/javascriptObjectLiterals/javascriptObjectLiterals.js
@@ -1,9 +1,19 @@
 // properties = keys + values
-const person = { firstName : 'Tuan', lastName: 'Khuat'}
+const person = { firstName: 'Tuan', lastName: 'Khuat' };
 
 const kitchenSink = {
-	favNum : 12312312,
-	isFunny: true,
-	colors : ['red', ' orange']
-}
+  favNum: 12312312,
+  isFunny: true,
+  colors: ['red', ' orange'],
+};
 
+console.log(kitchenSink);
+//arrays + objects
+
+const comments = [
+  { username: 'Tuan', text: 'hello my name is tuan', votes: 9 },
+  { username: 'park', text: 'anyanseyo', votes: 91 },
+  { username: 'Tuan', text: 'hello my name is tuan', votes: 9 },
+];
+
+console.log(comments[1].text);
---
+++ JS/JavaScriptBasicscourses/repeatingStuffWithLoops/jsLoops.js
@@ -0,0 +1,33 @@
+// loop allow us to repeat code
+// there are multiple types
+// 1.for.. loops
+// 2.while...loops
+// 3.for...of loops
+// 4.for...in loops
+
+let i = 25;
+
+do {
+  let result = i;
+  i -= 5;
+  console.log(result);
+} while (i >= 0);
+
+// expected 25 20 15 10 5 0
+
+const people = ['Tuan', 'Thanh', 'Park'];
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].toUpperCase());
+}
+
+for (person of people) {
+  console.log(person.toUpperCase());
+}
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`i is: ${i}`);
+  for (let j = 1; j < 4; j++) {
+    console.log(`      j is :${j}`);
+  }
+}
