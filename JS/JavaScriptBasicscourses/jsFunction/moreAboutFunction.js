dd8d6427c9d73e80054e53142790f182cb36e899
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-06-24 21:35:03 +0900

    ....

---
+++ JS/JavaScriptBasicscourses/jsFunction/moreAboutFunction.js
@@ -0,0 +1,16 @@
+// funtion scope
+// variable 'visibility'
+// The location where a variable is defined dictates where we have access to that variable
+
+function collectEggs(){
+    let totalEggs = 6;
+    console.log(totalEggs)
+}
+console.log(totalEggs)
+
+
+let radius = 8;
+if(radius > 0){
+    const PI = 3.14;
+    
+}
