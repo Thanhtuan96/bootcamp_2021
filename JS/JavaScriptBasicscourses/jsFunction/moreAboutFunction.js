db0e71dd3b2a8c7ca87dcfddab20e26904bfe0eb
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-06-28 21:25:28 +0900

    continuing funciton part

--- JS/JavaScriptBasicscourses/jsFunction/moreAboutFunction.js
+++ JS/JavaScriptBasicscourses/jsFunction/moreAboutFunction.js
@@ -2,15 +2,55 @@
 // variable 'visibility'
 // The location where a variable is defined dictates where we have access to that variable
 
-function collectEggs(){
-    let totalEggs = 6;
-    console.log(totalEggs)
-}
-console.log(totalEggs)
+// function collectEggs(){
+//     let totalEggs = 6;
+//     console.log(totalEggs)
+// }
+// console.log(totalEggs)
 
+// let radius = 8;
+// if(radius > 0){
+//     const PI = 3.14;
 
-let radius = 8;
-if(radius > 0){
-    const PI = 3.14;
-    
+// }
+
+//
+// higher order funciton
+//
+
+// that operate on/with other functions.PI
+// They can:
+//  Accept other funciton as arguments
+//  return a function
+
+// for example
+
+// function callTwice(func) {
+//   func();
+// }
+
+// function rollDie() {
+//   console.log(Math.floor(Math.random() * 6)+1);
+// }
+
+// rollDie();
+
+// function CallToRollDie(f,n){
+//     for(let i = 0; i<= n; i++){
+//         f()
+//     }
+// }
+
+// CallToRollDie(rollDie,10)
+
+function makeMysteryFunc() {
+  const rand = Math.random();
+  if (rand > 0.5) {
+    return function () {
+      console.log('Congrats, I am a good funciton!');
+      console.log('you win a million dollars');
+    };
+  } else {
+    prompt('try to close the browers');
+  }
 }
