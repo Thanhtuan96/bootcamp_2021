76c281cfa32c005271a19d31390c74ff4a212f97
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-06-29 21:34:14 +0900

    finsih basic function part

--- JS/JavaScriptBasicscourses/jsFunction/moreAboutFunction.js
+++ JS/JavaScriptBasicscourses/jsFunction/moreAboutFunction.js
@@ -31,7 +31,7 @@
 
 // function rollDie() {
 //   console.log(Math.floor(Math.random() * 6)+1);
-// }
+// }+
 
 // rollDie();
 
@@ -53,4 +53,60 @@ function makeMysteryFunc() {
   } else {
     prompt('try to close the browers');
   }
+}
+
+// defined method for function
+// We can add functions as properties on objects.Math
+// We call them methods
+
+// for example
+
+// const math = {
+//   multiply: function (x, y) {
+//     console.log(x * y);
+//     return x * y;
+//   },
+//   divide: function (x, y) {
+//     return x / y;
+//   },
+//   square: function (x) {
+//     return x * x;
+//   },
+// };
+
+// math.multiply(2, 3);
+
+// shorthand
+// We do this so often tat there's a new shorthand way of adding methods.
+
+// const math = {
+//   blah: 'Hi!',
+//   add(x, y) {
+//     return x + y;
+//   },
+//   multiply(x, y) {
+//     return x * y;
+//   },
+// };
+
+// keyword This in methods
+
+// use the keyword this to access other properties on the same objects
+
+// for example
+
+const person = {
+  first: 'Robert',
+  last: 'Herjavec',
+  fullName() {
+    return `${this.first} ${this.last}`;
+  },  
+};
+
+
+// Using Try/Catch
+try {
+    hello.toUpperCase();
+}catch {
+    console.log('errrorrrr!!!')
 }
