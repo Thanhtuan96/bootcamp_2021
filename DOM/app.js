cf85030c08bc7bafd933d83d8891e02580b12396
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-07-13 21:11:36 +0900

    moving on DOM event parts

---
+++ DOM/app.js
@@ -0,0 +1,20 @@
+// properties and methods
+// 1. classList
+// 2. getAttribute()
+// 3. setAttribute()
+// 4. appendChild()
+// 5. append()
+// 6. prepend()
+// 7. removeChild()
+// 8. remove()
+// 9. createElement()
+// 10. innerText()
+// 11. textContent()
+// 12. innerHTML()
+// 13. value()
+// 14. parentElement()
+// 15. children
+// 16. nextSibling()
+// 17. previousSibling()
+// 18. style()
+
