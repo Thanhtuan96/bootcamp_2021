dbc6a032ba72638b96891e3ed0fcdb90e21fbcad
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-08-04 21:13:59 +0900

    finished scorekepper excercise

--- DOM/scoreKeepper/app.js
+++ DOM/scoreKeepper/app.js
@@ -3,20 +3,56 @@ const p2btn = document.querySelector('#p2btn');
 const resetbtn = document.querySelector('#reset');
 let firstScore = document.querySelector('#firstScore');
 let secondScore = document.querySelector('#secondScore');
+let winningScoreSelect = document.querySelector('#selectedScore');
 
 let p1Score = 0;
 let p2Score = 0;
+let isGameOver = false;
+let winningScore = 0;
+
+winningScoreSelect.addEventListener('change', function () {
+  winningScore = parseInt(this.value);
+});
 
 p1btn.addEventListener('click', function () {
-  p1Score += 1;
-  firstScore.innerText = p1Score;
+  console.log(winningScore);
+  if (!isGameOver) {
+    p1Score += 1;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+      firstScore.classList.add('winner');
+    }
+    firstScore.innerText = p1Score;
+  }
 });
 p2btn.addEventListener('click', function () {
-  p2Score += 1;
-  secondScore.innerText = p2Score;
+  if (!isGameOver) {
+    p2Score += 1;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+      secondScore.classList.add('winner');
+    }
+    secondScore.innerText = p2Score;
+  }
 });
 
-resetbtn.addEventListener('click', function () {
-  secondScore.innerText = 0;
-  firstScore.innerText = 0;
-});
+resetbtn.addEventListener('click', reset);
+
+function isWin(a, b) {
+  let inputScore = document.querySelector('#selectedScore').value;
+  if (a >= inputScore) {
+    alert(`p1 won`);
+  } else if (b >= inputScore) {
+    alert(`p2 wonn`);
+  }
+}
+
+function reset() {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  firstScore.textContent = 0;
+  secondScore.textContent = 0;
+  firstScore.classList.remove('winner');
+  secondScore.classList.remove('winner');
+}
