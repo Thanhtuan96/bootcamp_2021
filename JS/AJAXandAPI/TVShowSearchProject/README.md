7dbd0eca5491b75a17510101946590abdb135b0a
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-08-20 21:43:39 +0900

    BOOTRAP/JS [AJAX, API, promise, call back, fetch, axios, XMLHttpRequest] finised TV show search project

---
+++ JS/AJAXandAPI/TVShowSearchProject/README.md
@@ -0,0 +1,10 @@
+# BELOW is the description about TV show search project
+
+tech used:
+- [HTML]()
+- [CSS]()
+- [JS]()
+
+using axios to fetch data from [tvmaze](https://www.tvmaze.com/api) api
+
+## make search, view chanel feature
---
+++ JS/AJAXandAPI/TVShowSearchProject/app.js
@@ -0,0 +1,52 @@
+const form = document.querySelector('#searchForm');
+const imgContainer = document.querySelector('.row');
+
+form.addEventListener('submit', (e) => {
+  let titles = [];
+  e.preventDefault();
+  const searchKeyword = form.elements.q.value;
+  getTvShowData(searchKeyword).then((data) => {
+    displayData(data);
+  });
+  form.elements.q.value = '';
+});
+
+const getTvShowData = async (key) => {
+  const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${key}`);
+  return res.data;
+};
+
+const displayData = (data) => {
+  const displayResults = data
+    .map((item) => {
+      return `
+
+            <div class="card col-sm-6" style="width:25%;">
+            <img src=${
+              item.show.image === null
+                ? 'http://placehold.jp/400x400.png'
+                : item.show.image.medium
+            }
+            class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${item.show.name}</h5>
+              <p class="card-text">${item.show.summary}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Language: ${item.show.language}</li>
+              <li class="list-group-item">Status: ${item.show.status}</li>
+              <li class="list-group-item">Genres:
+        ${item.show.genres.map((genre) => {
+          return ` ${genre}`;
+        })}  </li>
+            </ul>
+            <div class="card-body">
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>
+`;
+    })
+    .join('');
+  imgContainer.innerHTML = displayResults;
+};
