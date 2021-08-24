88c3ee6a28ee831e4d2483ce25d1654a047b0843
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-08-24 22:11:47 +0900

    BOOTCAMP/NodeJs: learn req.params, req.query, view engine EJS

--- NodeJs/firstApp/app.js
+++ NodeJs/firstApp/app.js
@@ -1,16 +1,38 @@
-const express = require('express')
-const app = express()
-
+const express = require('express');
+const app = express();
+const port = 3000;
 // any time we got request below code will run
-app.use((req,res)=>{
-    console.log('we go ta new request fron client')
-    res.send('hello we got your request')
-})
-
-// app.get('/',(req, res)=>{
-//     res.send('hello word')
+// app.use((req,res)=>{
+//     console.log('we go ta new request fron client')
+//     res.send('hello we got your request')
 // })
+app.set('view engine', 'ejs');
 
-app.listen(3000,()=>{
-    console.log('listening on port 3000!')
-})
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/employee', (req, res) => {
+  res.send('this is employee page');
+});
+
+app.get('/employees', (req, res) => {
+  res.send('this is all employees page');
+});
+
+app.get('/employee/:id', (req, res) => {
+  const { id } = req.params;
+  res.send(`this page is page of id: ${id} employee`);
+});
+
+app.get('/search', (req, res) => {
+  const { name, age } = req.query;
+  !name && !age
+    ? res.send('nothing to find')
+    : res.send(`${name} ${!age ? 'private' : age}`);
+  console.log({ name, age });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}!`);
+});
