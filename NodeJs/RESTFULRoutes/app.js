173e4ff918248e97feeaec931ac4713b581c9848
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-08-28 23:32:29 +0900

    BOOTCAMP/Nodejs : finish user Update enpoint

--- NodeJs/RESTFULRoutes/app.js
+++ NodeJs/RESTFULRoutes/app.js
@@ -1,26 +1,32 @@
 const express = require('express');
 var uniqid = require('uniqid');
+var methodOverride = require('method-override');
 const path = require('path');
 const app = express();
 
 const users = require('./usersData.json');
+const { get } = require('http');
 
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//GET: get index page
 app.get('/users', (req, res) => {
     res.render('index', { users });
 });
 
+//GET: create user template
 app.get('/users/new', (req, res) => {
     res.render('createUser');
 });
 
+//POST: Create new user
 app.post('/users/new', (req, res) => {
     const newUser = req.body;
     user.id = uniqid();
@@ -28,12 +34,33 @@ app.post('/users/new', (req, res) => {
     res.redirect('/users');
 });
 
+//GET: details for one specific user
 app.get('/users/:id', (req, res) => {
     const targetId = req.params.id;
-    const targetedUser = users.find((user) => (user.id = targetId));
+    const targetedUser = users.find((user) => user.id === targetId);
     res.render('userProfile', {
         user: targetedUser,
     });
+});
+
+//GET: update specific user form
+app.get('/users/:id/edit', (req, res) => {
+    const id = req.params.id;
+    const user = users.find((user) => user.id === id);
+    res.render('updateUser', { user });
+});
+
+app.patch('/users/:id', (req, res) => {
+    const { id } = req.params;
+    let foundUser = users.find((user) => user.id === id);
+
+    console.log(foundUser, req.body);
+
+    foundUser.email = req.body.email;
+    foundUser.name = req.body.name;
+    foundUser.age = parseInt(req.body.age);
+    foundUser.text = req.body.text;
+    res.redirect('/users');
 });
 
 app.listen(port, () => {
