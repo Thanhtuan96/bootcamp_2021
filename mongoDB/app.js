d685425526edd14dc8acef449f12f4680cad9957
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-08-31 22:28:41 +0900

    BOOTCAMP/Mongodb(Mongoose): finish mongo mongoose part

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -2,20 +2,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/YourTravel', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
 const port = 3000;
 
-const db = mongoose.connection;
+const mongoose = require('mongoose');
+mongoose
+    .connect('mongodb://localhost:27017/YourTravel', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Database connected..!');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.on('open', function () {
-    console.log('database connected...!');
-});
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.on('open', function () {
+//     console.log('database connected...!');
+// });
 
 const userSchema = new mongoose.Schema({
     name: String,
@@ -26,14 +33,30 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
+const productSchema = new mongoose.Schema({
+    name: String,
+    onSale: Boolean,
+    qty: Number,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+const bike = new Product({
+    name: 'Mountain',
+    onSale: true,
+    qty: 2,
+});
+
+User.find({})
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+
 // const Tuan = new User({
 //     name: 'Tuan',
 //     age: 25,
 //     email: 'thanhtuan220315@gmail.com',
 //     city: 'Ha Noi',
 // }).save();
-
-User.deleteMany({ name: 'Tuan' }).then((data) => console.log(data));
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
